6dbb875d6a152c60303f654dd54cd07a4b26bcb1
nelso <nelso@DESKTOP-84SADAK>
2021-09-30 09:50:49 +0300

    Add project files.

---
+++ TestAPI/Repositories/BookRepositoryBase.cs
@@ -0,0 +1,12 @@
+﻿namespace TestAPI.Repositories
+{
+    public class BookRepositoryBase
+    {
+
+        //public async Task<Book> Update(Book book)
+        //{
+        //    _context.Entry(book).State = EntityState.Modified;
+        //    await _context.SaveChangesAsync();
+        //}
+    }
+}
---
+++ TestAPI/Repositories/IBookRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestAPI.Models;
+
+namespace TestAPI.Repositories
+{
+    public interface IBookRepository
+    {
+        Task<IEnumerable<Book>> Get();
+        Task<Book> Get(int id);
+        Task<Book> Create(Book book);
+       // Task<Book> Update(Book book);
+        Task Delete(int id);
+    }
+}
